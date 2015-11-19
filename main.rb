38650fafb13666c0d2ce1e0dfcf6a028bb643324
Michelle Anderson <meobyte@gmail.com>
2015-11-18 20:53:25 -0800

    hand total helper

--- main.rb
+++ main.rb
@@ -5,6 +5,28 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'lem30zeW'
 
+helpers do
+  def hand_total(cards)
+    total = 0
+    ranks = cards.map { |card| card[0] }
+    aces_count = ranks.count("A")
+
+    ranks.each do |rank|
+      if rank == "A"
+        total += 11
+      else
+        total += (rank.to_i == 0 ? 10 : rank.to_i)
+      end
+    end
+
+    aces_count.times { total -= 10 if total > 21 }
+    total
+  end
+
+  def show_card(cards)
+
+  end
+end
 get '/' do
   if session[:player_name]
     redirect '/game'
@@ -28,8 +50,8 @@ end
 
 get '/game' do
   suits = ['H', 'D', 'C', 'S']
-  values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-  session[:deck] = suits.product(values).shuffle!
+  ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  session[:deck] = ranks.product(suits).shuffle!
 
   session[:dealer_cards] = []
   session[:player_cards] = []
