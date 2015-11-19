b292190e288e56fb8407c0dda6b20bfe38d6b177
Michelle Anderson <meobyte@gmail.com>
2015-11-18 20:21:41 -0800

    Set up initial game

--- main.rb
+++ main.rb
@@ -6,7 +6,11 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'lem30zeW'
 
 get '/' do
-    redirect '/set_name' unless session[:player_name]
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/set_name'
+  end
 end
 
 get '/set_name' do
@@ -19,9 +23,20 @@ post '/set_name' do
 end
 
 get '/bet' do
-  erb :mytemplate
+
 end
 
 get '/game' do
+  suits = ['H', 'D', 'C', 'S']
+  values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  2.times do
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+  end
+
   erb :game
 end
