494c3f2c3bd806841ed112df7f7a78fef490a887
Michelle Anderson <meobyte@gmail.com>
2015-11-18 19:23:38 -0800

    save username in session and redirect appropriately

--- main.rb
+++ main.rb
@@ -4,3 +4,24 @@ require 'sinatra'
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'lem30zeW'
+
+get '/' do
+    redirect '/set_name' unless session[:player_name]
+end
+
+get '/set_name' do
+  erb :set_name
+end
+
+post '/set_name' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/bet' do
+  erb :mytemplate
+end
+
+get '/game' do
+  erb :game
+end
